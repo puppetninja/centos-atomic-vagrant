6641e021da8fbaa4802fe154b68800235b4b75cd
Chao Zhang <charles.zhangzs@gmail.com>
2017-11-27 23:38:28 +0100

    disable signature-verification for docker

--- scripts/node/config_docker.sh
+++ scripts/node/config_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DOCKER_CONF="/etc/sysconfig/docker"
 
-sed -i "s@OPTIONS.*@OPTIONS='--registry-mirror=http://192.168.122.10:5000 --selinux-enabled --log-driver=journald'@" ${DOCKER_CONF}
+sed -i "s@OPTIONS.*@OPTIONS='--registry-mirror=http://192.168.122.10:5000 --selinux-enabled --log-driver=journald --signature-verification=false'@" ${DOCKER_CONF}
